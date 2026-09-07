5c9cb0d54a25c492d2dfb9ab2a330aa2ef9ff558
tomaseliasfontes <tomaseliasfontes@gmail.com>
2026-09-06 23:50:42 -0300

    Initial commit

---
+++ DotNetDependencies/Program.cs
@@ -0,0 +1,22 @@
+﻿using Humanizer;
+
+Console.WriteLine("Quantities:");
+HumanizeQuantity();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantity()
+{
+    Console.WriteLine("case".ToQuantity(0)); // "no cases"
+    Console.WriteLine("case".ToQuantity(1)); // "1 case"
+    Console.WriteLine("case".ToQuantity(5)); // "5 cases"
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize()); // "yesterday"
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize()); // "2 hours ago"
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize()); // "1 day"
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize()); // "16 days"
+}
