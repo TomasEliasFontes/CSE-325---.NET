477ab3ae9b3fbe13b15d0fe78b232de441e83a71
tomaseliasfontes <tomaseliasfontes@gmail.com>
2026-09-18 18:45:18 -0300

    Add Bootstrap Reboot RTL CSS file for improved right-to-left support

---
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
